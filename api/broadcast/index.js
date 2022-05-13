675ee7e2d39f9e74186e60ec35d1ad384586990c
Kevin McDonnell <kevin@mcd79.com>
2022-05-13 22:51:25 +0100

    Fixing broadcast with logging

--- api/broadcast/index.js
+++ api/broadcast/index.js
@@ -2,28 +2,35 @@ const uuid = require('uuid');
 // use this to update scores regularly every 30 seconds
 
 module.exports = function (context) {
+  console.log('Broadcast triggered');
   var documents = context.bindings.scoreDocuments;
+  console.log('Score docs found: ' + context.bindings.scoreDocuments);
   if (context.bindings.showDocuments.length != 1) {
     throw '';
   }
+  console.log('Show docs found: ' + context.bindings.showDocuments);
   var show = context.bindings.showDocuments[0];
 
   let scores = {};
   for (var i=0; i < show.Panellists.length; i++) {
     scores[show.Panellists[i].Title] = 0;
   }
+  console.log(scores);
 
-
+  console.log("Getting scores by panellist");
   for (var i = 0; i < documents.length; i++) {
     var document = documents[i];
     scores[document.recipient] += document.scoreChange;
   }
 
+  console.log("Updating scores by panellist");
   for (var i=0; i < show.Panellists.length; i++) {
     show.Panellists[i].TotalScore = scores[show.Panellists[i].Title];
   }
 
+  console.log("Saving show");
   context.bindings.scoreDocument = show;
+  console.log("Sending out the update to client");
   context.bindings.signalRMessages = [
     {
       target: "newMessage",
