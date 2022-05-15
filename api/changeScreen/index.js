73c9879699453d939d75d7de0b9417fdb5037003
Kevin McDonnell <kevin@mcd79.com>
2022-05-15 10:40:30 +0100

    Saving show status

--- api/changeScreen/index.js
+++ api/changeScreen/index.js
@@ -5,6 +5,16 @@ const uuid = require('uuid');
 module.exports = function (context, req) {
     const screenName = req.body.screenName;
     console.log(`ScreenName: ${screenName}`);
+
+    if (context.bindings.showDocuments.length != 1) {
+        throw '0 or more than 1 show docs found';
+    }
+    
+    var show = context.bindings.showDocuments[0];
+    show.CurrentScreen = screenName;
+
+    context.bindings.showDocument = JSON.stringify(show);
+
     context.bindings.signalRMessages = [{
         "target": "screenChanged",
         "arguments": [ screenName ]
